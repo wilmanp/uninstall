055a9bd61f824c2f23071f668843e97a5bcf327b
Wilman Pimentel Beltran <110548805+wilmanp@users.noreply.github.com>
2023-08-21 15:09:51 -0400

    Update unninstall_study.sh

--- unninstall_study.sh
+++ unninstall_study.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+
 echo "Type the study name and then hit enter(return):	"
 read study
 
 #removes enviroment folder
+# machete command
+#find ~/Desktop/$desktop -exec rm -rf {} \; 
+#friendly command
 conda remove -n $study --all -y
 
+#desktop launcher
 desktop=$study".sh"
 
 #Deleting desktop launcher
-find ~/Desktop/$desktop -exec rm -rf {} \; 
+if [-d ~/Desktop/$desktop]: then
+	find ~/Desktop/$desktop -exec rm -rf {} \; 
+	echo "desktop launcher deleted!"
+else echo "desktop launcher not found."
+fi
 
 
 echo "$study was succesfully uninstalled"

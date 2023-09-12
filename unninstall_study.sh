01815fb019c1bc04ab71e1bfff4b111776f4e261
Wilman Pimentel Beltran <110548805+wilmanp@users.noreply.github.com>
2023-09-12 09:51:44 -0400

    Add files via upload

--- unninstall_study.sh
+++ unninstall_study.sh
@@ -1,35 +1,53 @@
 #!/bin/bash
 
 
-echo "Type the study name and then hit enter(return):	"
-read study
+while true; do
 
-#Removes enviroment folder
-# machete command
-#find ~/miniconda3/envs/$study -exec rm -rf {} \; 
-#friendly command
-conda remove -n $study --all -y
+	echo "Type the study name and then hit enter(return):	"
+	read study
 
-#Desktop launcher
-desktop=$study".sh"
-inst="install_"$desktop
+	#Removes enviroment folder
+	# machete command
+	#find ~/miniconda3/envs/$study -exec rm -rf {} \; 
+	#friendly command
 
-#Deleting desktop launcher
-echo "Looking for launcher..."
-if [ -e ~/Desktop/$desktop ]; then
-	find ~/Desktop/$desktop -exec rm -rf {} \; 
-	echo "desktop launcher deleted!"
-else
-	echo "desktop launcher not found..."
-fi
+	if [ -e ~/miniconda3/envs/$study ]; then
+		conda remove -n $study --all -y
+	elif [ -e /miniconda/envs/$study ]; then
+		conda remove -n $study --all -y
+	else
+		echo "Study Name was not found in the computer. Please double check the name and try again."
+		#exit
+	fi		
 
-echo "Looking for installation file: $inst"
-if [ -e ~/Downloads/$inst ]; then
-	find ~/Downloads/$inst -exec rm -rf {} \; 
-	echo "installation scrip deleted!"
-else
-	echo "installation script not found..."
-fi
+	#Desktop launcher
+	desktop=$study".sh"
+	inst="install_"$desktop
 
+	#Deleting desktop launcher
+	echo "Looking for launcher..."
+	if [ -e ~/Desktop/$desktop ]; then
+		find ~/Desktop/$desktop -exec rm -rf {} \; 
+		echo "desktop launcher deleted!"
+	else
+		echo "desktop launcher not found..."
+	fi
+
+	echo "Looking for installation file: $inst"
+	if [ -e ~/Downloads/$inst ]; then
+		find ~/Downloads/$inst -exec rm -rf {} \; 
+		echo "installation scrip deleted!"
+	else
+		echo "installation script not found..."
+	fi
+
+
+	echo "Press enter/retun to try again. Press Esc to exit."
+    read -s -n 1 key
+    if [ "$key" == $'\x1b' ]; then
+        break  # salir con escape
+    fi
+
+done
 
 echo "Finished!"

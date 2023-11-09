dfc6231f75e403c69ab63112d06942ee98b42c58
Wilman Pimentel Beltran <110548805+wilmanp@users.noreply.github.com>
2023-11-09 14:35:34 -0500

    Add files via upload

---
+++ cleanup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#Deletes files (in the download folder) that are 3 months or older.
+while true; do
+	#statements
+
+
+read -p "This script will delete files 90 days or older, including installation files and datasets. Would you like to proceed? (y/n):" yesno
+
+
+case $yesno in 
+	[yY]* ) echo "Ok! deleting files...";
+		find ~/Downloads/ -mtime +90 -exec rm -rf {} \;
+		rm -rf ~/.local/share/Trash/*
+		break;;
+	[nN]* ) echo "Exiting script...No files were deleted.";
+		exit;;
+		* ) echo "Invalid response. Enter y or n.";;
+
+esac
+
+done
+
+echo "Completed!"

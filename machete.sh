35a9730d972722f30d4733816c8af3f904d01502
Wilman Pimentel Beltran <110548805+wilmanp@users.noreply.github.com>
2023-11-09 10:30:22 -0500

    Add files via upload

---
+++ machete.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+
+	echo "Type the study name and then hit enter(return):	"
+	read study
+
+	#Desktop launcher
+	desktop=$study".sh"
+	inst="install_"$desktop
+
+	#------Removes enviroment folder
+	# machete command
+	#find ~/miniconda3/envs/$study -exec rm -rf {} \; 
+	
+	if [ -e ~/miniconda3/envs/$study ]; then
+		find ~/miniconda3/envs/$study -exec rm -rf {} \; 
+	elif [ -e /miniconda/envs/$study ]; then
+		find ~/miniconda/envs/$study -exec rm -rf {} \; 
+	else
+		echo "Study Name was not found in the computer. Please double check the name and try again."
+		exit
+	fi	
+
+	#------Friendly command
+
+	#if [ -e ~/miniconda3/envs/$study ]; then
+	#	conda remove -n $study --all -y
+	#elif [ -e /miniconda/envs/$study ]; then
+	#	conda remove -n $study --all -y
+	#else
+	#	echo "Study Name was not found in the computer. Please double check the name and try again."
+		#exit
+	#fi		
+
+	#Deleting desktop launcher
+	echo "Looking for launcher..."
+	if [ -e ~/Desktop/$desktop ]; then
+		find ~/Desktop/$desktop -exec rm -rf {} \; 
+		echo "desktop launcher deleted!"
+	else
+		echo "desktop launcher not found..."
+	fi
+
+	echo "Looking for installation file: $inst"
+	if [ -e ~/Downloads/$inst ]; then
+		find ~/Downloads/$inst -exec rm -rf {} \; 
+		echo "installation scrip deleted!"
+	else
+		echo "installation script not found..."
+	fi
+
+
+	echo "Press enter/retun to try again. Press Esc to exit."
+    read -s -n 1 key
+    if [ "$key" == $'\x1b' ]; then
+        break  # salir con escape
+    fi
+
+
+echo "Finished!"
